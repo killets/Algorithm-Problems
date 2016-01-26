4af7e8bfa1bb1547d8c19c2dcf78f2612cc844b0
J.G <killets@users.noreply.github.com>
2016-01-26 14:37:06 -0500

    Update 89. Gray Code.cpp
    public List<Integer> grayCode(int n) {
        List<Integer> result = new LinkedList<>();
        for (int i = 0; i < 1<<n; i++) result.add(i ^ i>>1);
        return result;
    }
    The idea is simple. G(i) = i^ (i/2).

--- 89. Gray Code.cpp
+++ 89. Gray Code.cpp
@@ -1,7 +1,12 @@
+// 别人算法，直接用公式ans[i] = i^(i/2);
+//（假設以二進制為0的值做為格雷碼的0）
+//G：格雷码 B：二进制码
+//G(N) = (B(n)/2) XOR B(n)
+
 // dp, code[i] = change one bit of n bits of code[i-1], (code^=(1<<x)), if not used
 // set code[] from 0 to 2^n, done.
 // Note: 2 techniques used here, first change x-th bit of a number, num=num^(1<<x)
-// second, total number, nth power of 2, 2<<(n-1)
+// second, total number, nth power of 2, 2<<(n-1),or 1<<n
 
 class Solution {
 public:
