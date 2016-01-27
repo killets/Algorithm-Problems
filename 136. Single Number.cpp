d865e3fd8b94a64b67ce683f2af9765b8f894a0d
J.G <killets@users.noreply.github.com>
2016-01-27 09:33:45 -0500

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,22 @@
+// insert and toggle one element by element in hash map, then check the single one
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+      unordered_map<int, bool> hash;
+      for(int i=0; i<nums.size(); i++) {
+          std::unordered_map<int, bool>::iterator it = hash.find(nums[i]); 
+          if (it != hash.end())
+            it->second = false;
+          else
+            hash.insert(std::make_pair(nums[i],true));
+      }
+      
+      for(auto & it: hash) {
+          if(it.second==true){
+              return it.first;
+          }
+      }
+      return 0; // for empty input
+    }
+};
