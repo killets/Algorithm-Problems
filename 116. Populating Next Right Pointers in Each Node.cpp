868df18ad670f13674efdaa97578ac19d8df31af
J.G <killets@users.noreply.github.com>
2016-03-22 14:39:06 -0400

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- 116. Populating Next Right Pointers in Each Node.cpp
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -7,6 +7,50 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
+ //update：我写的BFS方法中用到cnt来记数，但cnt到2^n时认为到头了
+ //在学了while中套for的level order traversal之后，可以这样做：
+ 
+ //注意这样错了。因为stack的一层还没读完就被新的节点抢先了，还是要用queue
+ void connect(TreeLinkNode* root) {
+   if(root==nullptr) return;
+   stack<TreeLinkNode*>s;
+   s.push(root);
+   
+   while(!s.empty()){
+    int n= s.size();
+    TreeLinkNode* pre = nullptr;
+    for(int i=0; i<n; i++) {
+     TreeLinkNode* tmp = s.top(); s.pop();
+     tmp->next = pre;
+     pre= tmp;
+     if(tmp->left) s.push(tmp->left);
+     if(tmp->right) s.push(tmp->right);
+    }
+   }
+  
+ }
+ 
+
+ void connect(TreeLinkNode* root) {
+   if(root==nullptr) return;
+   queue<TreeLinkNode*>q;
+   q.push(root);
+   
+   while(!q.empty()){
+    int n= q.size();
+    TreeLinkNode* pre = nullptr;
+    for(int i=0; i<n; i++) {
+     TreeLinkNode* tmp = q.front(); q.pop();
+     if(pre) //NOTE!
+        pre->next = tmp;
+     pre= tmp;
+     if(tmp->left) q.push(tmp->left);
+     if(tmp->right) q.push(tmp->right);
+    }
+   }
+ }
+
+ 
  //有点像level遍历，BFS
 class Solution {
 public:
