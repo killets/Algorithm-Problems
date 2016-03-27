e964830a5150e2ef20033783abb03dcab9427da9
J.G <killets@users.noreply.github.com>
2016-03-27 13:03:17 -0400

    Create Nsum小结.cpp

---
+++ basic/Nsum小结.cpp
@@ -0,0 +1,5 @@
+nsum的问题的基础是2sum， 2sum的解法在geek中提到是两种: hashmap和while（L<r）双指针；
+3sum的问题就是在2sum外加一层for，这样构成比如 for while(l<r)的结构；
+4sum的问题继续加一层for，for for while（l<r）的结构’
+//貌似大家更喜欢用while双指针解法》hashmap的解法
+所以，这类问题就是双指针问题
