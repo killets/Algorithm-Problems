3f54585a3217b8dd796fbc66ce474986e6627053
J.G <killets@users.noreply.github.com>
2016-04-20 20:40:04 -0400

    Create Egg Drop.cpp

---
+++ Egg Drop.cpp
@@ -0,0 +1,13 @@
+参考 https://brilliant.org/wiki/egg-dropping/
+
+
+Since we need to minimize the number of trials in worst case, we take the maximum of two cases. We consider the max of above two cases for every floor and choose the floor which yields minimum number of trials.
+
+  k ==> Number of floors
+  n ==> Number of Eggs
+  eggDrop(n, k) ==> Minimum number of trails needed to find the critical
+                    floor in worst case.
+  eggDrop(n, k) = 1 + min{max(eggDrop(n - 1, x - 1), eggDrop(n, k - x)): 
+                 x in {1, 2, ..., k}}
+                 
+                 
