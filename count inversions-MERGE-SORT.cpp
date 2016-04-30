7cde4fb05e4d8e1889b8173354833a3ef4541151
J.G <killets@users.noreply.github.com>
2016-04-30 16:41:51 -0400

    Create count inversions-MERGE-SORT.cpp

---
+++ count inversions-MERGE-SORT.cpp
@@ -0,0 +1,11 @@
+http://www.cs.umd.edu/class/fall2009/cmsc451/lectures/Lec08-inversions.pdf
+[img] http://geeksforgeeks.org/wp-content/uploads/2010/01/inv_count3.GIF
+
+
+MainQustion()
+ cnt1= MainQuestion(left-half)
+ cnt2 = MainQuestion(right-half)
+ cnt3 = merge(left-half, right-half);
+ return cnt1+cnt2+cnt3
+ 
+ 结构上和merge sort是一样的
