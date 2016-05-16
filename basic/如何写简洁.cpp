06745373b4eafa2216f5008f1cf4882504167147
J.G <killets@users.noreply.github.com>
2016-05-15 23:25:25 -0400

    Update 如何写简洁.cpp

--- basic/如何写简洁.cpp
+++ basic/如何写简洁.cpp
@@ -8,3 +8,10 @@
 
 20 把合法情况列出来，然后其它都用 return false； 另外没有用case 而是if else if, 这样不写break，适合于较短的程序块，比如一句话。
 
+131 Fewest palindrome partition： track最小的方法，避免另外设置一个变量记录最小值，但要求c[][]的赋值代价不能比临时变量高
+                // Make a cut at every possible localtion starting from i to j,
+                // and get the minimum cost cut.
+                C[i][j] = INT_MAX;
+                for (k=i; k<=j-1; k++)
+                    C[i][j] = min (C[i][j], C[i][k] + C[k+1][j]+1);
+      常规写法是另外再设一个临时变量，最后赋给目标
